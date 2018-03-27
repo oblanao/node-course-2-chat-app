f654429188d72160a282fa834b4bba31cbb13de5
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2018-03-27 15:30:49 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-    socket.emit('createMessage', {
-        from: 'oana',
-        text: 'Hey',
-    });
+    
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -16,13 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'box',
-        text: 'Salutare',
-        createdAt: 123123
-    });
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     })
     socket.on('disconnect', () => {
         console.log('Client disconnected');
